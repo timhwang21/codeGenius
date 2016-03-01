f4b1a6a8b861df3751b57543eb726b2891e2c5ce
timhwang21 <timhwang21@gmail.com>
2016-03-01 13:45:34 -0800

    fix seed file to not conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -114,7 +114,7 @@ end
     Annotation.create!(
       author_id: author_id,
       snippet_id: i + 1,
-      line_idx: j+rand(3),
+      line_idx: j*2+rand(2),
       title: Faker::Hipster.paragraph(1),
       body: Faker::Hipster.paragraph(3),
       upvotes: rand(1..100),
