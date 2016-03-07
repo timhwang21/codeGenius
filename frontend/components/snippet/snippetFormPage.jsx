3a75b6965bba8d8129349e9322292c72efe8ad45
timhwang21 <timhwang21@gmail.com>
2016-03-06 22:51:50 -0800

    add scroll top to new snippet page

--- frontend/components/snippet/snippetFormPage.jsx
+++ frontend/components/snippet/snippetFormPage.jsx
@@ -1,7 +1,15 @@
 var React = require('react');
 var SnippetForm = require('./snippetForm');
 
+var animateScrollTop = function() {
+  $("html, body").animate({scrollTop: "0px"}, 200);
+};
+
 var SnippetFormPage = React.createClass({
+  componentDidMount: function() {
+    animateScrollTop();
+  },
+
   render: function() {
     return(
       <section className="snippet-index">
